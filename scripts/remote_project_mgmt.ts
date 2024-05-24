ee284fe1659abe47433944d3f2fb2665040b7ff5
re4388 <re4388@gmail.com>
2024-05-24 21:42:28 +0800

    WIP

--- scripts/remote_project_mgmt.ts
+++ scripts/remote_project_mgmt.ts
@@ -6,51 +6,56 @@ import clipboard from 'clipboardy'
 import { escAndQToExit } from '../util/escToExit.ts'
 import { errLog } from '../util/errorLog.ts'
 
-// escAndQToExit()
+enum OPTION {
+  clone_repo = 'clone repo',
+  list = 'list',
+  show_path = 'show path',
+  show_size = 'show size'
+}
 
 const answer = await select({
   message: 'Select...',
   choices: [
     {
-      name: 'list',
-      value: 'list',
-      description: 'show repositories'
-    },
-    {
-      name: 'clone repo',
-      value: 'clone repo',
+      name: OPTION.clone_repo,
+      value: OPTION.clone_repo,
       description: 'get remote repo'
     },
     {
-      name: 'show path',
-      value: 'show path',
+      name: OPTION.list,
+      value: OPTION.list,
+      description: 'show repositories'
+    },
+    {
+      name: OPTION.show_path,
+      value: OPTION.show_path,
       description: 'show path for remote repo'
     },
     {
-      name: 'show size',
-      value: 'show size',
+      name: OPTION.show_size,
+      value: OPTION.show_size,
       description: 'show size for remote repo'
     }
   ]
 })
 
-const gitClonePath = '/Users/re4388/project/personal/git-clone-pjt'
+const Git_Clone_Path = '/Users/re4388/project/personal/git-clone-pjt'
 
 switch (answer) {
-  case 'show path': {
-    console.log('path: ', gitClonePath)
+  case OPTION.show_path: {
+    console.log('path: ', Git_Clone_Path)
     break
   }
-  case 'list': {
+  case OPTION.list: {
     try {
-      await copyProjectToPath()
+      await copyPathToClipboard()
     } catch (error) {
       errLog(error)
       throw error
     }
     break
   }
-  case 'clone repo': {
+  case OPTION.clone_repo: {
     try {
       await getRemoteRepo()
     } catch (error) {
@@ -60,13 +65,13 @@ switch (answer) {
 
     break
   }
-  case 'show size': {
+  case OPTION.show_size: {
     // await $`cd ${gitClonePath} && gdu-go`
     await $`sh /Users/re4388/project/personal/my-github-pjt/dotfiles/zsh/check_git_clone_pjt.sh`
 
     const txt = 'or..you can go to directory and goes into interactive mode with "du"'
     console.log(chalk.green(txt))
-    const cd_to_fullPath = 'cd ' + gitClonePath
+    const cd_to_fullPath = 'cd ' + Git_Clone_Path
     clipboard.writeSync(cd_to_fullPath)
     console.log(`${chalk.blue(cd_to_fullPath)} is copied to clipboard!`)
     break
@@ -77,12 +82,12 @@ switch (answer) {
   }
 }
 
-async function copyProjectToPath() {
-  const res = await $`cd ${gitClonePath} && eza -al | fzf`
+async function copyPathToClipboard() {
+  const res = await $`cd ${Git_Clone_Path} && eza -al | fzf`
   // TODO: 這裡  use arrow down key will error
-  const fileNameWithSlashN = res.stdout.split(' ').slice(-1)
-  const fileName = fileNameWithSlashN[0].split('\n').slice(-2, -1)
-  const fullPath = 'cd ' + gitClonePath + '/' + fileName
+  const fileNameWithSlash = res.stdout.split(' ').slice(-1)
+  const fileName = fileNameWithSlash[0].split('\n').slice(-2, -1)
+  const fullPath = 'cd ' + Git_Clone_Path + '/' + fileName
   clipboard.writeSync(`${fullPath}`)
   console.log(`${chalk.bgWhite.bold(fullPath)} is copied to clipboard!`)
 }
